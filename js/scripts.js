e5ba0e9f80e86776672b3f9083a0a68ba6ee6049
EarthmanAman <hashimathman@gmail.com>
2021-02-06 11:25:25 +0300

    initial

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$(".bxslider").bxSlider({
+			video: true,
+			auto:true,
+			autoHover:true,
+			pause: 9000,
+			mode: "horizontal",
+			moveSlides: 1,
+			slideMargin: 10,
+			infiniteLoop:true,
+			minSlides: 1,
+			maxSlides: 1,
+			speed: 1700,
+
+	})
+	
+
+});
